e6b4161d8a532641e15e1d4ddfa9821ea47122c0
septanto <s.s.adi.nugroho@gmail.com>
2018-08-25 18:22:37 +0700

    req 4 & add jacoco

--- src/main/java/com/septanto/tddjava/ch3tictactoe/TicTacToe.java
+++ src/main/java/com/septanto/tddjava/ch3tictactoe/TicTacToe.java
@@ -17,8 +17,10 @@ public class TicTacToe {
         checkAxis(y);
         lastPlayer = nextPlayer();
         setBox(x, y, lastPlayer);
-        if (isWin()) {
+        if (isWin(x, y)) {
             return lastPlayer + " is the winner";
+        } else if (isDraw()) {
+            return "The result is draw";
         }
         return "No Winner";
     }
@@ -44,22 +46,30 @@ public class TicTacToe {
         return 'X';
     }
 
-    private boolean isWin() {
+    private boolean isWin(int x, int y) {
         int playerTotal = lastPlayer * 3;
-        char diagonal1 = '\0';
-        char diagonal2 = '\0';
+        char horizontal, vertical, diagonal1, diagonal2;
+        horizontal = vertical = diagonal1 = diagonal2 = '\0';
         for (int i = 0; i < SIZE; i++) {
+            horizontal += board[i][y - 1];
+            vertical += board[x - 1][i];
             diagonal1 += board[i][i];
             diagonal2 += board[i][SIZE - i - 1];
-            if (board[0][i] + board[1][i] + board[2][i] == playerTotal) {
-                return true;
-            } else if (board[i][0] + board[i][1] + board[i][2] == playerTotal) {
-                return true;
-            }
         }
-        if (diagonal1 == playerTotal || diagonal2 == playerTotal) {
+        if (horizontal == playerTotal || vertical == playerTotal || diagonal1 == playerTotal || diagonal2 == playerTotal) {
             return true;
         }
         return false;
+    }
+
+    private boolean isDraw() {
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                if (board[x][y] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- src/test/java/com/septanto/tddjava/ch3tictactoe/TicTacToeSpec.java
+++ src/test/java/com/septanto/tddjava/ch3tictactoe/TicTacToeSpec.java
@@ -92,4 +92,18 @@ public class TicTacToeSpec {
         String actual = ticTacToe.play(3, 1); // X
         assertEquals("X is the winner", actual);
     }
+
+    @Test
+    public void whenAllBoxesAreFilledThenDraw() {
+        ticTacToe.play(1, 1);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(1, 3);
+        ticTacToe.play(2, 1);
+        ticTacToe.play(2, 3);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(3, 1);
+        ticTacToe.play(3, 3);
+        String actual = ticTacToe.play(3, 2);
+        assertEquals("The result is draw", actual);
+    }
 }
