c601bee37a8575e3001cb89f7a4c734ec8ad71a3
septanto <s.s.adi.nugroho@gmail.com>
2018-09-11 15:00:01 +0700

    add store moves to mongo

--- src/main/java/com/septanto/tddjava/ch3tictactoe/TicTacToeCollection.java
+++ src/main/java/com/septanto/tddjava/ch3tictactoe/TicTacToeCollection.java
@@ -2,6 +2,7 @@ package com.septanto.tddjava.ch3tictactoe;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import com.septanto.tddjava.ch3tictactoe.mongo.TicTacToeBean;
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
 
@@ -17,6 +18,26 @@ public class TicTacToeCollection {
 
     public TicTacToeCollection() throws UnknownHostException {
         DB db = new MongoClient().getDB("tic-tac-toe");
-        mongoCollection = new Jongo(db).getCollection("bla");
+        mongoCollection = new Jongo(db).getCollection("game");
     }
+
+    public boolean saveMove(TicTacToeBean bean) {
+        try {
+            getMongoCollection().save(bean);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public boolean drop() {
+        try {
+            getMongoCollection().drop();
+            return true;
+        } catch (Exception e ) {
+            return false;
+        }
+    }
+
 }
--- src/test/java/com/septanto/tddjava/ch3tictactoe/TicTacToeCollectionSpec.java
+++ src/test/java/com/septanto/tddjava/ch3tictactoe/TicTacToeCollectionSpec.java
@@ -1,15 +1,85 @@
 package com.septanto.tddjava.ch3tictactoe;
 
+import com.mongodb.MongoException;
+import com.mongodb.WriteResult;
+import com.septanto.tddjava.ch3tictactoe.mongo.TicTacToeBean;
+import org.jongo.MongoCollection;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.net.UnknownHostException;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class TicTacToeCollectionSpec {
+
+    TicTacToeCollection collection;
+    TicTacToeBean bean;
+    MongoCollection mongoCollection;
+
+    @Before
+    public void before() throws UnknownHostException {
+        collection = spy(new TicTacToeCollection());
+        bean = new TicTacToeBean(3, 2, 1, 'Y');
+        mongoCollection = mock(MongoCollection.class);
+    }
+
     @Test
-    public void whenInstantiatedThenMongoHasDbNameTicTacToe() throws UnknownHostException {
-        TicTacToeCollection collection = new TicTacToeCollection();
+    public void whenInstantiatedThenMongoHasDbNameTicTacToe() {
         assertEquals("tic-tac-toe", collection.getMongoCollection().getDBCollection().getDB().getName());
+    }
+
+    @Test
+    public void whenInstantiatedThenMongoCollectionHasNameGame() {
+        assertEquals("game", collection.getMongoCollection().getName());
+    }
+
+    @Test
+    public void whenSaveMoveThenInvokeMongoCollectionSave() {
+        doReturn(mongoCollection).when(collection).getMongoCollection();
+        collection.saveMove(bean);
+        verify(mongoCollection, times(1)).save(bean);
+    }
+
+    @Test
+    public void whenSaveMoveThenReturnTrue() {
+        doReturn(mongoCollection).when(collection).getMongoCollection();
+        assertTrue(collection.saveMove(bean));
+    }
+
+    @Test
+    public void givenExceptionWhenSaveMoveThenReturnFalse() {
+        doThrow(new MongoException("Bla")).when(mongoCollection).save(any(TicTacToeBean.class));
+        doReturn(mongoCollection).when(collection).getMongoCollection();
+        assertFalse(collection.saveMove(bean));
+    }
+
+    @Test
+    public void whenDropThenInvokeMongoCollectionDrop() {
+        doReturn(mongoCollection).when(collection).getMongoCollection();
+        collection.drop();
+        verify(mongoCollection).drop();
+    }
+
+    @Test
+    public void whenDropThenReturnTrue() {
+        doReturn(mongoCollection).when(collection).getMongoCollection();
+        assertTrue(collection.drop());
+    }
+
+    @Test
+    public void givenExceptionWhenDropThenReturnFalse() {
+        doThrow(new MongoException("Bla")).when(mongoCollection).drop();
+        doReturn(mongoCollection).when(collection).getMongoCollection();
+        assertFalse(collection.drop());
     }
 }
